260afafe31d22ba0de7d978b6eab233015e93b36
krishanmurari <krishanmurari0596@gmail.com>
2024-04-09 17:15:05 +0530

    Sign In & Sign UP & login page & header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-81 py-4 px-4 bg-gradient-to-b from-black">
+      <img className="w-12" src="logo192.png" alt="logo" />
+    </div>
+  );
+};
+
+export default Header;
